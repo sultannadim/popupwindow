9b4de4cd490038ce82acd90ef7bb52db5f08373f
sultan nadim <sultan5218@gmail.com>
2021-12-27 14:13:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-document.querySelector('.btn').addEventListener('click', function(){
-	document.querySelector('.popup_bg').style.display = 'flex';
+// document.querySelector('.btn').addEventListener('click', function(){
+// 	document.querySelector('.popup_bg').style.display = 'flex';
+// });
+
+// document.querySelector('.close').addEventListener('click', function(){
+// 	document.querySelector('.popup_bg').style.display = 'none';
+// });
+
+
+$(document).ready(function(){
+
+$( ".btn" ).click(function() {
+  $( ".full_pop" ).fadeIn( "slow", function() {
+  });
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-	document.querySelector('.popup_bg').style.display = 'none';
+$( ".close" ).click(function() {
+  $( ".full_pop" ).fadeOut( "slow", function() {
+  });
+});
+
 });
